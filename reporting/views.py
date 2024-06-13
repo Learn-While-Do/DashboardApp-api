d0736deb7234c9c0a87ebac98ab790ba8d0e09e2
Learn-While-Do <LearnWhileDo@gmail.com>
2024-06-13 21:21:16 +0200

    End of Chapter 4

--- reporting/views.py
+++ reporting/views.py
@@ -1,20 +1,38 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import viewsets
 
-from reporting.models import Order
-from reporting.serializers import OrderSerializer
+from reporting.models import Category, Customer, Order, Product, Supplier
+from reporting.serializers import CategorySerializer, CustomerSerializer, OrderSerializer, ProductSerializer, SupplierSerializer
 
-class ReportingViewSet(APIView):
-    def get(self, request):
-
-        answer = {'id': "42", 'name': "question"}
-
-        return Response(answer)
         
 class OrderViewSet(viewsets.ModelViewSet):
 
     serializer_class = OrderSerializer
 
     def get_queryset(self):
-        return Order.objects.all().order_by('-created_time')
+        return Order.objects.all().order_by('-order_date')
+    
+class CategoryViewSet(viewsets.ModelViewSet):
+
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+
+    serializer_class = CustomerSerializer
+
+    def get_queryset(self):
+        return Customer.objects.all().order_by('country', 'last_name')
+    
+
+class SupplierViewSet(viewsets.ModelViewSet):
+    
+    serializer_class = SupplierSerializer
+
+    def get_queryset(self):
+        return Supplier.objects.all().order_by('country', 'company_name')
+    
+class ProductViewSet(viewsets.ModelViewSet):
+    
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
